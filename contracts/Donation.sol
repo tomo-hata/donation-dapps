ae3e1dfdf6e9fe156804d68f12baa68423ce41ca
tomo-hata <tomo-hata@users.noreply.github.com>
2018-07-29 18:52:42 +0900

    modified contract.

--- contracts/Donation.sol
+++ contracts/Donation.sol
@@ -1,96 +1,97 @@
 pragma solidity ^0.4.23;
-// 2018/7/23現在、修正中。
+/* 23/7/2018 being modified */
+// Donation Contract is coded using Solidity, to send donation amount to reciever.
+// A contract owner can raise contributions as ether from public,
+// and then he can send reciver total amount for donation.
+// Notes: this contract is not fixed, so you should not use this contract in Ethereum mainnet.
 contract Donation {
-/* グローバル変数 */
-    address public toDonAddr;   //寄付先アドレス
-    address owner; // コントラクトのオーナー(作成者)
-    uint public collectAmount;  //集めた寄付金額
-    uint public deadline;       //寄付募集期限
-    Donar[] public donars;      //寄付者（配列化）
+/* grobal variable */
+    address public toDonAddr;   //address to receive donation
+    address owner; //contract owner(initial deploy)
+    uint public collectAmount;  //total amount for donation
+    uint public deadline;       //deadline for donation
+    Donar[] public donars;      //donars information(array list)
     struct Donar {
         address addr;
         uint amount;
     }
-/* プライベート変数 */
-    uint8 private flg_sendFix = 0;  //初期値：0, 送金済み：１
-    uint8 private flg_sendCancel = 0; //初期値：0, キャンセル済み：１
-/* イベント監視を定義 */
-    //送金処理
+/* private variable */
+    uint8 private flg_sendFix = 0;  //initial value:0, finish to send amount:1
+    uint8 private flg_sendCancel = 0; //initial value:0, finish to cancel:1
+/* events` */
+    //send
     event sendCoin(address sender,address receiver, uint amount);
-    //キャンセル処理
+    //cancel
     event cancelDonation(address sender, address receiver, uint amount, uint8 flg);
-/* メソッド */
-    //初期化メソッド    
+/* functions */
+    //initialize
     constructor (address _toDonAddr,uint _duration) public {
         toDonAddr = _toDonAddr;
         owner = msg.sender;
         deadline = block.timestamp + _duration * 1 minutes;
     }
-    //コントラクト作成者のみに実行実行権限付与
+    //if you use this modifier, only contract owner can execute.
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
-    //deadlineまでは入金可能
+    //if you use this modifier, we can send amount to this contract before deadline.
     modifier before_deadline {
         require(block.timestamp < deadline);
         _;
     }
-    //deadline以降は出金可能
+    //if you use this modifier, we can transfer amount to receiver after deadline.
     modifier after_deadline {
         require(block.timestamp >= deadline);
         _;
     }
-    //無名メソッド(etherがコントラクト宛に送金される度に呼び出される)
+    //anonymous function
+    //if you send amounts to this contract, the anoymous function is always called. 
     function () public payable before_deadline{
-        //受取予定者アドレス自身は寄付できない
+        //receiver address cannot send this contract amount.
         require(msg.sender != toDonAddr);
-        //終了時間より前なら寄付送金をコントラクト宛に送金可能
+        //we can send amount before deadline.
         require(block.timestamp < deadline);
         uint amount = msg.value;
-        //寄付者と寄付金額を配列として保存
+        //add array list for donar information
         donars[donars.length++] = Donar({addr: msg.sender, amount:amount});
         collectAmount += amount;
     }
-    //送金処理
+    //function to send (valid after deadline )
     function SendCoin() public onlyOwner after_deadline returns(bool _trueorfalse, uint8 _flg_sendFix) {
-        //キャンセル済みでなければ処理継続
+        //confirm if not canceled.
         require (flg_sendCancel == 0);
-        //寄付金額が0以下であれば送金処理を実行しない
+        //confirm total amount is greater than zero.
         require (collectAmount > 0);
         toDonAddr.transfer(collectAmount);
         flg_sendFix = 1;
-        //イベント発行
+        //event ignition to send transaction
         emit sendCoin(this,toDonAddr, collectAmount);
-        //この辺りに寄付者宛にお礼用のユニークなトークン(ERC721ベースとか)を送付する処理を作成検討中
-        //別のトークン用コントラクトを作成し、そのコントラクトを呼び出すやり方も検討中(2018/7/23)
-        /* トークン配布用処理を入れる(検討中) */
         return (true, flg_sendFix);
     }
-    //寄付キャンセル処理処理（※送金処理実施前まで有効）
+    //function to cancel
     function CancelDonation() public onlyOwner returns(bool _trueorfalse){
-        //未送金であれば処理継続
+        //confirm if not sent.
         require (flg_sendFix == 0);
-        //寄付者に返金処理
+        //refund donars amount.
         for (uint i = 0; i < donars.length; ++i){
             donars[i].addr.transfer(donars[i].amount);
         }
-        //キャンセルフラグをキャンセル済みにする
         flg_sendCancel = 1;
-        //イベント発行
+        //event ignition to cancel transaction
         emit cancelDonation(this,toDonAddr, collectAmount,flg_sendCancel);
         return true;
     }
-/* 情報取得系 */
-    //コントラクトオーナー取得
+/* function to get information */
+    //get contract owner
     function getOwner() public view returns (address) {
         return owner;
     }
-    //寄付金合計額取得
+    //get current total amount
     function getCollectAmount() public view returns (uint) {
         return collectAmount;
     }
-    //block.timestamp取得
+    //get current block.timestamp
     function getBlockTimestamp() public view returns(uint) {
         return block.timestamp;
     }
